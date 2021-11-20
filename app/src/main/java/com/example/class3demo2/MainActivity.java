df3fb3a3239deeab02e44c38cac0953a8f587241
linoyelimeleh <linoy.elimeleh@solaredge.com>
2021-11-20 16:39:25 +0200

    student details frame

--- app/src/main/java/com/example/class3demo2/MainActivity.java
+++ app/src/main/java/com/example/class3demo2/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.example.class3demo2.model.Model;
+import com.example.class3demo2.model.Student;
+
 public class MainActivity extends AppCompatActivity {
     EditText nameEt;
     EditText idEt;
@@ -38,9 +41,13 @@ public class MainActivity extends AppCompatActivity {
         String name = nameEt.getText().toString();
         String id = idEt.getText().toString();
         boolean flag = cb.isChecked();
+        Model.instance.addStudent(new Student(name, id, flag));
         Log.d("TAG", "saved name:" + name + " id:" + id + " flag:" + flag);
-        setContentView(R.layout.activity_student_list);
+
+        // Go to the student view
+        // setContentView(R.layout.activity_student_list);
         Intent intent = new Intent(this, StudentDetails.class);
+        intent.putExtra("student_name", new String(name));
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/class3demo2/StudentDetails.java
+++ app/src/main/java/com/example/class3demo2/StudentDetails.java
@@ -1,24 +1,39 @@
 package com.example.class3demo2;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.class3demo2.model.Model;
 import com.example.class3demo2.model.Student;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class StudentDetails extends AppCompatActivity {
+    List<Student> data;
+    Student currentStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        // Search the student in the list
+        String nameStudent = getIntent().getStringExtra("student_name");
+        //  String nameStudent="name 1"
+        data = Model.instance.getAllStudents();
+        //Optional<Student> studentToShow = (data.stream().filter(x -> x.getName() == nameStudent).findFirst());
+        //todo chek what if we have more then one
+        // currentStudent = studentToShow.get();
+
 
     }
 }
